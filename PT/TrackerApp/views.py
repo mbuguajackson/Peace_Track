4a22f7745d84c8753499063d6ee0bad9c8b1cdfc
mbuguajackson <jak.mbugua@gmail.com>
2025-05-14 20:39:34 +0300

    index

--- PT/TrackerApp/views.py
+++ PT/TrackerApp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+from .models import conflict_data, ssd_level_0
+from django.views.generic import TemplateView
+
+
 # Create your views here.
+class HomePageView(TemplateView):
+    template_name = "index.html"
+
